440813ef83adb46e6990937c483c54b61d99e6c4
Martin Sandgren <martin.sandgren.2@volvocars.com>
2017-11-27 13:43:13 +0100

    sync 2017-11-27T13:43+01:00

--- content/bake.py
+++ content/bake.py
@@ -1,31 +1,38 @@
 import sys, json
 from glob import glob
-from colorama import Fore, Style
+from colorama import Fore, Style, init as colorama_init
 
 def main():
     if len(sys.argv) < 3:
         print('[in directory] [out directory]')
         sys.exit(1)
 
+    colorama_init()
+
     dirRead = sys.argv[1]
     dirWrite = sys.argv[2]
-    print('Compiling content from %s to %s' % (dirRead, dirWrite))
+    print(
+            ('Compiling content from '+
+            Fore.YELLOW+'%s'+Style.RESET_ALL+' to '+
+            Fore.YELLOW+'%s'+Style.RESET_ALL) % (dirRead, dirWrite))
 
-    game = {
+    game_json = {
             'start': 'intro',
-            'intro': {
+            'script': {
                 }
             }
+    game_json = json.dumps(game_json)
 
-    game_js = 'GAME = %s' % json.dumps(game)
+    game_json_js = 'GAME = %s' % game_json
 
-    write_file(dirWrite, 'game.js', game_js)
-    print('Done.')
+    write_file(dirWrite, 'game.json', game_json)
+    write_file(dirWrite, 'game.js', game_json_js)
+    print(Fore.GREEN + 'Baking done!' + Style.RESET_ALL)
 
 def write_file(dir, path, content):
     path = '%s/%s' % (dir, path)
     print(
-            (Fore.CYAN + 'Writing %d b to %s.' + Style.RESET_ALL) %
+            ('Writing %d b to '+Fore.CYAN+'%s'+Style.RESET_ALL+'.') %
             (sys.getsizeof(content), path))
 
     fd = open(path, 'wb')
